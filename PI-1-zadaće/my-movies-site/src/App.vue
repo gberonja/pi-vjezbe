becc1e4e5fe5a08c78c1e4f6f65226bb373711a5
Gaketz <gabriel.beronja@gmail.com>
2025-03-10 12:12:42 +0100

    Zadaća br. 1

---
+++ PI-1-zadaće/my-movies-site/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="flex flex-col items-center min-h-screen bg-gradient-to-br from-emerald-950 to-indigo-950 text-teal-100 dark:from-emerald-900 dark:to-indigo-900 dark:text-teal-200"
+  >
+    <h1 class="text-[60px] my-2 text-center sm:text-[50px] md:text-[60px]">
+      Pozdrav, Ja sam
+      <span class="text-cyan-400 dark:text-cyan-300">Gabriel Beronja</span>
+    </h1>
+    <div
+      class="w-[80%] sm:w-[80%] md:w-[1024px] border-2 opacity-75 rounded-full bg-gray-500 dark:bg-gray-700"
+    ></div>
+    <div class="flex flex-col items-center w-full px-4">
+      <div class="flex w-full justify-center">
+        <h2
+          class="text-[24px] sm:text-[20px] text-teal-100 dark:text-gray-200 my-5 w-[80%] sm:w-[80%] md:w-[1024px]"
+        >
+          Moji omiljeni filmovi:
+        </h2>
+      </div>
+      <div
+        class="flex flex-col sm:flex-row sm:flex-wrap gap-4 sm:gap-8 md:flex-row justify-center"
+      >
+        <div class="flex flex-col gap-y-2 sm:w-[100%] md:w-[320px]">
+          <div
+            class="w-full h-[200px] sm:h-[300px] md:h-[480px] rounded-md bg-gray-300 dark:bg-gray-600 flex justify-center items-center text-6xl text-gray-500 dark:text-gray-400"
+          >
+            Poster
+          </div>
+          <p
+            class="mx-auto text-2xl text-teal-100 dark:text-gray-200 text-center"
+          >
+            Naslov filma
+          </p>
+        </div>
+        <div class="flex flex-col gap-y-2 sm:w-[100%] md:w-[320px]">
+          <div
+            class="w-full h-[200px] sm:h-[300px] md:h-[480px] rounded-md bg-gray-300 dark:bg-gray-600 flex justify-center items-center text-6xl text-gray-500 dark:text-gray-400"
+          >
+            Poster
+          </div>
+          <p
+            class="mx-auto text-2xl text-teal-100 dark:text-gray-200 text-center"
+          >
+            Naslov filma
+          </p>
+        </div>
+        <div class="flex flex-col gap-y-2 sm:w-[100%] md:w-[320px]">
+          <div
+            class="w-full h-[200px] sm:h-[300px] md:h-[480px] rounded-md bg-gray-300 dark:bg-gray-600 flex justify-center items-center text-6xl text-gray-500 dark:text-gray-400"
+          >
+            Poster
+          </div>
+          <p class="text-[24px] text-center text-teal-100 dark:text-gray-200">
+            Naslov filma
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
